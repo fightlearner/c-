7a80c712b5d01d739dec8f54546fee25612976f5
flightlearner <2864079090@qq.com>
2024-11-27 21:08:42 +0800

    冒泡排序

--- bubble/bubble/main.c
+++ bubble/bubble/main.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
 
-void bubble(int a[], int b)
+int comp(void* e1, void* e2)
+{
+	return (*((int*)e2) - *((int*)e1));
+}
+
+void sweap(char* a, char* b, int width)
+{
+	for (int i = 0;i < width;i++)
+	{
+		char temp = *(a + i);
+		*(a + i) = *(b + i);
+		*(b + i) = temp;
+	}
+}
+
+
+
+void bubble(void* arr, int b, int width, int (*comp)(void* e1, void* e2))
 {
 	if (b > 1)
 	{
@@ -8,26 +25,31 @@ void bubble(int a[], int b)
 		int flag = 1;
 		for (int i = 0;i < b - 1;i++)
 		{
-			if (a[i] > a[i + 1])
+			if (comp((char*)arr + i * width, (char*)arr + (i + 1) * width))
+			{
+				sweap((char*)arr + i * width, (char*)arr + (i + 1) * width, width);
+				flag = 0;
+			}
+			/*if (a[i] > a[i + 1])
 			{
 				temp = a[i + 1];
 				a[i + 1] = a[i];
 				a[i] = temp;
 				flag = 0;
-			}
+			}*/
 		}
 		if (flag == 0)
 		{
-			bubble(a, b - 1);
+			bubble(arr, b - 1, width, comp);
 		}
 	}
 }
 
 int main()
 {
-	int arr[] = { 1,2,3,6,7567,2,4,3,24,56,7,8};
+	int arr[] = { 1,2,3,4,5,6,7,8,9,10,11,12};
 	int sz = sizeof(arr) / sizeof(arr[0]);
-	bubble(arr, sz);
+	bubble(arr, sz, sizeof(arr[0]), comp);
 	for (int i = 0;i < sz;i++)
 	{
 		printf("%d ", arr[i]);
